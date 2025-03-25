96e0cd5799e029ab06722a82e8df5e7d0bc6796f
Spencer Palmer <monkies989@gmail.com>
2025-03-25 02:26:46 -0600

    Cleaned up test cases and added new performance tests and an analysis.

--- TestCases.cs
+++ TestCases.cs
@@ -16,36 +16,43 @@ public class TestCases
         item.Enqueue(3);
         item.Enqueue(4);
         item.Enqueue(5);
-        item.Enqueue(6);
 
         Console.WriteLine($"Size: {item.Size()}");
 
-        item.GetItem();
+        for(int i = 0; i < item.Size(); i++)
+        {
+            Console.WriteLine(item.toString(i));
+        }
         // Expected output:
-        // Size: 6
+        // Size: 5
         // 1
         // 2
         // 3
         // 4
         // 5
-        // 6
 
         Console.WriteLine("==== Test Case 2 ====");
-        // Add then remove item from queue
-        item = new LLQueue<int>();
-        item.Enqueue(1);
-        item.Enqueue(2);
-        item.Enqueue(3);
-        item.Enqueue(4);
-        item.Enqueue(5);
-        item.GetItem();
+        // Remove item from queue returning the removed item to the console. Remove another item this time with no return before 
+        // removing another item with a return. Repeat this one more times before removing the rest of the items and triggering
+        // the try/catch statement that posts "Queue is empty." to the console.
+        for(int i = 0; i < item.Size(); i++)
+        {
+            Console.WriteLine(item.toString(i));
+        }
 
         Console.WriteLine(item.Dequeue());
         item.Dequeue();
         Console.WriteLine(item.Dequeue());
         item.Dequeue();
         Console.WriteLine(item.Dequeue());
-        Console.WriteLine(item.Dequeue());
+        try
+        {
+            Console.WriteLine(item.Dequeue());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
         // Expected output:
         // 1
         // 2
@@ -58,20 +65,33 @@ public class TestCases
         // Queue is empty.
 
         Console.WriteLine("==== Test Case 3 ====");
-        // Add item then peek at the first item in queue
-        item = new LLQueue<int>();
+        // Add items then peek at the first item in queue, then remove two more items and peek at the next item before removing
+        // the rest of the items and triggering the try/catch statment.
         item.Enqueue(1);
         item.Enqueue(2);
         item.Enqueue(3);
         item.Enqueue(4);
         item.Enqueue(5);
-        item.GetItem();
+        for(int i = 0; i < item.Size(); i++)
+        {
+            Console.WriteLine(item.toString(i));
+        }
         Console.WriteLine(item.Peek());
         item.Dequeue();
         item.Dequeue();
         Console.WriteLine(item.Peek());
-        item.DequeueAll();
-        Console.WriteLine(item.Peek());
+        while(item.Size() != 0)
+        {
+            item.Dequeue();
+        }
+        try
+        {
+            Console.WriteLine(item.Peek());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
         // Expected output:
         // 1
         // 2
@@ -83,19 +103,21 @@ public class TestCases
         // Queue is empty.
 
         Console.WriteLine("==== Test Case 4 ====");
-        // Add item then check to see if the queue contains an item
-        item = new LLQueue<int>();
+        // Add items to queue and display all the items in the queue, then check to see if the queue contains specific items. 
+        // Remove one of the items from the queue and check for it again.
         item.Enqueue(1);
         item.Enqueue(2);
         item.Enqueue(3);
-        item.GetItem();
+        for(int i = 0; i < item.Size(); i++)
+        {
+            Console.WriteLine(item.toString(i));
+        }
 
         Console.WriteLine(item.Contains(3));
         Console.WriteLine(item.Contains(4));
         Console.WriteLine(item.Contains(1));
         item.Dequeue();
         Console.WriteLine(item.Contains(1));
-
         // Expected output:
         // 1
         // 2
@@ -106,6 +128,31 @@ public class TestCases
         // False
 
         Console.WriteLine("==== Test Case 5 ====");
-        PerformanceMeasurement.MeasurePerformance();
+        // Performance tests to calculate speed of each main function's execution.
+        Console.WriteLine("==== Performance Test 10 Items ====");
+        PerformanceMeasurement.MeasurePerformance10();
+        Console.WriteLine();
+        Console.WriteLine("==== Performance Test 100 Items ====");
+        PerformanceMeasurement.MeasurePerformance100();
+        Console.WriteLine();
+        Console.WriteLine("==== Performance Test 1,000 Items ====");
+        PerformanceMeasurement.MeasurePerformance1000();
+        Console.WriteLine();
+        Console.WriteLine("==== Performance Test 10,000 Items ====");
+        PerformanceMeasurement.MeasurePerformance10000();
+        Console.WriteLine();
+        Console.WriteLine("==== Performance Test 100,000 Items ====");
+        PerformanceMeasurement.MeasurePerformance100000();
+        Console.WriteLine();
+        Console.WriteLine("==== Performance Test 1,000,000 Items ====");
+        PerformanceMeasurement.MeasurePerformance1000000();
     }
+    /// <summary>
+    /// Based on the performance tests I can clearly see that Enqueue, Peek, and Dequeu are all O(1) complexity because they maintain
+    /// a consistance execution speed until the items reach 1,000,000 where they begin to jump to double digit speeds.
+    /// 
+    /// The Contains function on the other hand appears to be an O(n) complexity because it's execution time clearly increases as the
+    /// number of items/inputs increases. Once we reach 1,000,000 items we see a massive spike in time it takes to execute and causes
+    /// the program to take a considerable amount of time to progress.
+    /// </summary>
 }
