acf251bc1e884c1145ba2a0bc7bc1bfc6b87dc88
Spencer Palmer <monkies989@gmail.com>
2025-03-25 02:26:07 -0600

    Cleaned up code and added comments

--- LLQueue.cs
+++ LLQueue.cs
@@ -1,10 +1,8 @@
-using System.Linq.Expressions;
-
 namespace prove_12;
 
-public class LLQueue<T> where T: new()
+public class LLQueue<T>
 {
-    // Create Variable
+    // Create a LinkedList for the queue and a variable to keep track of queue size
     private LinkedList<T> _queue = new LinkedList<T>();
     private int _size;
 
@@ -14,25 +12,26 @@ public class LLQueue<T> where T: new()
         _size = 0;
     }
     
-    // Add item to Queue
+    // Add item to Queue and increment the size to corresponding to the increase of items in the queue
     public void Enqueue (T item)
     {
         _queue.AddLast(item);
         _size++;
     }
 
-    // Removes an item from the queue and returns the value of the removed item.
-    public object Dequeue() 
+    // Removes an item from the front of the queue and returns the value of the removed item. Reduce the value of _size to correspond
+    // to the removal of an item from the queue. Using the RemoveFirst function in the LinkedList library we can remove the first item
+    // in the queue and move the pointer to the next item.
+    public T Dequeue() 
     {
-        // If the _front variable and _rear variable are the same that means the queue is empty so throw an exception to the terminal.
-        if (_size == 0)
+        // If _size variable is zero that means the queue is empty so throw an exception to the terminal.
+        if (IsEmpty())
         {
-            Console.WriteLine("Queue is empty.");
-            return null;
+            throw new Exception("Queue is empty.");
         }
         else
         {
-            // put the item at the front of the queue into a temp variable and then change the location of the frot of the queue to
+            // put the item at the front of the queue into a temp variable and then change the location of the front of the queue to
             // simulate removing the item, finally return the temp variable.
             T item = _queue.First();
             _queue.RemoveFirst();
@@ -41,30 +40,13 @@ public class LLQueue<T> where T: new()
         }
     }
 
-    public void DequeueAll()
-    {
-        if (_size != 0)
-        {
-            while(_size != 0)
-            {
-                _queue.RemoveFirst();
-                _size--;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Queue is empty.");
-        }
-    }
-
     // Looks at the item at the front of the queue.
-    public object Peek()
+    public T Peek()
     {
         // If the _front variable and _rear variable are the same that means the queue is empty so throw an exception to the terminal.
-        if (_size == 0)
+        if (IsEmpty())
         {
-            Console.WriteLine("Queue is empty.");
-            return null;
+            throw new Exception("Queue is empty.");
         }
         else
         {
@@ -76,28 +58,14 @@ public class LLQueue<T> where T: new()
     // Check to see if the queue contains a certain item then return it.
     public bool Contains(T item)
     {
-        bool results = false;
-        if (_queue.Contains(item))
-            results = true;
-        return results;
+        return _queue.Contains(item);
     }
 
     // Gets all items from the queue fron front to back.
-    public void GetItem()
+    public String toString(int i)
     {
-        // If the _size is zero that means the queue is empty so throw an exception to the terminal.
-        if(_size == 0)
-        {
-            Console.WriteLine("Queue is empty.");
-        }
-        else
-        {
-            // Loop through every item in the queue starting from the front and ending at the back.
-            for(int i = 0; i < _size; i++)
-            {
-                    Console.WriteLine(_queue.ElementAt(i));
-            }
-        }
+        // Convert item in _queue to a string and return it.
+        return $"{_queue.ElementAt(i)}";
     }
 
     // Checks with the _size variable to see if the queue is empty.
@@ -110,10 +78,5 @@ public class LLQueue<T> where T: new()
     public int Size()
     {
         return _size;
-    }
-
-    private object ReturnNull()
-    {
-        return "Queue is full.";
     }
 }
